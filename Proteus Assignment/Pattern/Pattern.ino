69a148e502b23b220de12037487fe579c7369472
Kaye Engbino <engbinop@gmail.com>
2020-10-28 00:02:14 +0800

    3 diff seq from 3 diff buttons

--- Proteus Assignment/Pattern/Pattern.ino
+++ Proteus Assignment/Pattern/Pattern.ino
@@ -9,12 +9,33 @@ void setup()
 void loop()
 {
   //i = blue LED, j = green
+  if(digitalRead(8) == 1)
+  {
     for(int i = 0, j = 4; i < 4, j <= led; i++, j++)
     {
         digitalWrite(i,LOW);
         digitalWrite(j,LOW);
-        delay(100);
+        delay(500);
         digitalWrite(i,HIGH);
         digitalWrite(j,HIGH);
      }
+  } 
+  else if(digitalRead(9) == 1)
+  {
+    for(int i = 0; i <= led; i++)
+    {
+      digitalWrite(i,LOW);
+      delay(500);
+      digitalWrite(i,HIGH);
+    }
+  }
+  else if(digitalRead(10) == 1)
+  {
+    for(int i = led; i >= 0; i--) 
+    {
+      digitalWrite(i,HIGH);
+      delay(500);
+      digitalWrite(i,LOW);
+    }
+  }
 } 
