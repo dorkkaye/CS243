98b7314e73f123d11ce72f2d1885c283386cc837
Kaye Engbino <engbinop@gmail.com>
2020-12-12 19:41:23 +0800

    CPU with ROM

--- CPU with ROM/ROM/ROM.ino
+++ CPU with ROM/ROM/ROM.ino
@@ -1,77 +1,97 @@
-int address=0, data = 0;
-int c = 0, m = 0;
-int program []={0b1001000000000000,
-         0b1001001000000001,
-         0b1001010000000101,
-         0b1110000000000001,
-         0b1110110000001001,
-         0b1011100000000111,
-         0b1011000000000011,
-         0b1011000000000111};
+int address=0,data=0;
+int c=0,i=0,m=0;
 
-void setup() 
-{
+unsigned int program[256]={0b1001000000000000,
+            0b1001001000000001,
+            0b1001010000000101,
+            0b1110000000000001,
+            0b1110110000001001,
+            0b1011100000000111,
+            0b1011000000000011,
+            0b1011000000000111};
+
+//variables for auto generated program instructions;
+int programInstrCount = 8;
+int instructionRegister; // 5 bit
+unsigned int newProgram; //11 bit
+int exist = 0;
+
+void setup() {
   // put your setup code here, to run once:
-    pinMode (0, INPUT);
-    pinMode (1, INPUT);
-    pinMode (2, INPUT);
-    pinMode (3, INPUT);
-    pinMode (4, INPUT);
-    pinMode (5, INPUT);
-    pinMode (6, INPUT);
-    pinMode (7, INPUT);
-    
-//set data bus
-    pinMode (8, OUTPUT);
-    pinMode (9, OUTPUT);
-    pinMode (10, OUTPUT);
-    pinMode (11, OUTPUT);
-    pinMode (12, OUTPUT);
-    pinMode (13, OUTPUT);
-    pinMode (14, OUTPUT);
-    pinMode (15, OUTPUT);
-    pinMode (16, OUTPUT);
-    pinMode (17, OUTPUT);
-    pinMode (18, OUTPUT);
-    pinMode (19, OUTPUT);
-    pinMode (20, OUTPUT);
-    pinMode (21, OUTPUT);
-    pinMode (22, OUTPUT);
-    pinMode (23, OUTPUT);
+pinMode(0,INPUT);
+pinMode(1,INPUT);
+pinMode(2,INPUT);
+pinMode(3,INPUT);
+pinMode(4,INPUT);
+pinMode(5,INPUT);
+pinMode(6,INPUT);
+pinMode(7,INPUT);
+// set data  bus
+pinMode(8,OUTPUT);
+pinMode(9,OUTPUT);
+pinMode(10,OUTPUT);
+pinMode(11,OUTPUT);
+pinMode(12,OUTPUT);
+pinMode(13,OUTPUT);
+pinMode(14,OUTPUT);
+pinMode(15,OUTPUT);
+pinMode(16,OUTPUT);
+pinMode(17,OUTPUT);
+pinMode(18,OUTPUT);
+pinMode(19,OUTPUT);
+pinMode(20,OUTPUT);
+pinMode(21,OUTPUT);
+pinMode(22,OUTPUT);
+pinMode(23,OUTPUT);
+  
+  for(c=8;c<256;c++){
+    instructionRegister = c % 16; // 1001
+    instructionRegister = instructionRegister & 0x1F; // 11111 && 1001 = 11001
+    instructionRegister = instructionRegister << 12;
+    exist = 0;
+    newProgram = c & 0x7FF; //00000001000
+    newProgram = newProgram & 0x200; //00X00001000
+    newProgram = instructionRegister | newProgram; //11001 00001000000
+
+    //avoid duplication from existing program code
+    for(i = 0; i < 8; i++){
+      if(newProgram == program[i])
+        exist = 1;
+    }
+    if(exist !=1)
+      program[c] = newProgram;
+    else
+      program[c] = 0b1000000000000000;
+  }
 }
 
 void getAddress()
 {
   m=1;
+   for(c=0;c<8;c++)
+   {
+      if(digitalRead(c)==1)
+        address=(address|m); //0-255
+      m=m*2;
+   }
   
-  for (c=0;c<8;c++)
-  {
-    if (digitalRead(c) == 1)
-    address= (address|m);
-    m=m*2;
-  }
 }
+void sendData(){
+    data=program[address];
+     m=1;
+ for(c=8;c<24;c++){
+  if((data&m)==0)
+     digitalWrite(c,0);
+  else 
+     digitalWrite(c,1);
 
-void sendData()
-{
-  data=program[address];
-  m=1;
+  m=m*2;
+ }
+ }
 
-  for (c=8; c<24; c++)
-  {
-    if ((data&m) == 0)
-      digitalWrite (c,0);
-    else
-      digitalWrite (c,1);
 
-     m=m*2;
-  }
-}
-
-void loop() 
-{
+void loop() {
   // put your main code here, to run repeatedly:
-  getAddress();
-  sendData();
-
+    getAddress();
+    sendData();
 }
--- CPU with ROM/cpu/cpu.ino
+++ CPU with ROM/cpu/cpu.ino
@@ -1,188 +1,233 @@
-int PC, IR, AR, DR, ZF;
-int c = 0, m=1, D, I, RL, RR;
-int GEN[] = {0,0,0};
+int PC,IR,AR,DR,ZF;
+int c=0,m=1,D,I,RL,RR;
+int GEN[]={0,0,0}; //W,FR1,FR2
 
-void setup() 
-{
+void setup() {
   // put your setup code here, to run once:
-  PC = IR = AR= DR= ZF = 0;
-    pinMode (0, OUTPUT);
-    pinMode (1, OUTPUT);
-    pinMode (2, OUTPUT);
-    pinMode (3, OUTPUT);
-    pinMode (4, OUTPUT);
-    pinMode (5, OUTPUT);
-    pinMode (6, OUTPUT);
-    pinMode (7, OUTPUT);
-    
-// set data bus
-    pinMode (8, INPUT);
-    pinMode (9, INPUT);
-    pinMode (10, INPUT);
-    pinMode (11, INPUT);
-    pinMode (12, INPUT);
-    pinMode (13, INPUT);
-    pinMode (14, INPUT);
-    pinMode (15, INPUT);
-    pinMode (16, INPUT);
-    pinMode (17, INPUT);
-    pinMode (18, INPUT);
-    pinMode (19, INPUT);
-    pinMode (20, INPUT);
-    pinMode (21, INPUT);
-    pinMode (22, INPUT);
-    pinMode (23, INPUT);
+   PC=IR=AR=DR=ZF=0;
+  pinMode(0,OUTPUT);
+  pinMode(1,OUTPUT);
+  pinMode(2,OUTPUT);
+  pinMode(3,OUTPUT);
+  pinMode(4,OUTPUT);
+  pinMode(5,OUTPUT);
+  pinMode(6,OUTPUT);
+  pinMode(7,OUTPUT);
+  // set data  bus
+  pinMode(8,INPUT);
+  pinMode(9,INPUT);
+  pinMode(10,INPUT);
+  pinMode(11,INPUT);
+  pinMode(12,INPUT);
+  pinMode(13,INPUT);
+  pinMode(14,INPUT);
+  pinMode(15,INPUT);
+  pinMode(16,INPUT);
+  pinMode(17,INPUT);
+  pinMode(18,INPUT);
+  pinMode(19,INPUT);
+  pinMode(20,INPUT);
+  pinMode(21,INPUT);
+  pinMode(22,INPUT);
+  pinMode(23,INPUT);
 
-    pinMode (24, OUTPUT);
-    pinMode (25, OUTPUT);
-    pinMode (26, OUTPUT);
-    pinMode (27, OUTPUT);
-    pinMode (28, OUTPUT);
-    pinMode (29, OUTPUT);
-    pinMode (30, OUTPUT);
-    pinMode (31, OUTPUT);
-    pinMode (32, OUTPUT);
-    pinMode (33, OUTPUT);
-    pinMode (34, OUTPUT);
-    pinMode (35, OUTPUT);
+
+  //debug Instruction Register
+  pinMode(24,OUTPUT);
+  pinMode(25,OUTPUT);
+  pinMode(26,OUTPUT);
+  pinMode(27,OUTPUT);
+  pinMode(28,OUTPUT);
+  pinMode(29,OUTPUT);
+  pinMode(30,OUTPUT);
+  pinMode(31,OUTPUT);  
+  pinMode(32,OUTPUT);
+  pinMode(33,OUTPUT);
+  pinMode(34,OUTPUT);
+  pinMode(35,OUTPUT);
 }
 
-void fetch1()
-{
-   AR=PC;
-//send data of AR to address bus
-    m=1;
-    
-  for (c=0; c<8;c++)
-  {
-    if ((AR&m)==0)
-      digitalWrite(c,0);
+void fetch1(){
+  AR=PC; 
+  // send data of AR to address bus
+  m=1;
+  for(c=0;c<8;c++){
+    if((AR&m)==0)
+       digitalWrite(c,0);
     else 
-      digitalWrite (c,1);
-      m=m*2;
-   }
+       digitalWrite(c,1);
+    m=m*2;
+  }
 }
 
-void fetch2()
-{
-   PC=PC+1;
-   DR=0;
-   m=1;
-   
-   for (c=8; c<24; c++)
-   {
-    if (digitalRead(c)==1)
-      DR = (DR|m);
-      m=m*2;
-   }
+void fetch2(){
+  PC=PC+1;
+  DR=0;
+  m=1;
+
+  for(c=8;c<24;c++){
+    if(digitalRead(c)==1)
+       DR=(DR|m);
+    m=m*2;    
+  }
 }
 
 void fetch3()
 {
-   IR = (DR>>11);
-   IR = IR&0x000F;
-   m=1;
-   
-    for(c=24; c<35;c++){
-      if ((IR&m)==0)
-        digitalWrite(c,0);
-      else 
-        digitalWrite(c,1);
-        m=m*2;
-     }
+    IR = DR>>11;
+    IR = IR&0xF;
+
+    m = 1;
+    for(c=24; c<=35;c++){
+      if((IR&m)==0)
+        digitalWrite(c,0); 
+      else
+        digitalWrite(c,1); 
+     m=m*2; 
+    }
 }
 
-      void MOVR(){}
 
-      void MOVI()
-      {
-        I= (DR&0x00FF);
-        D=(DR>>9);
-        D=(D&0xFFF3);
-        GEN[D] = I;
-      }
+
+void MOVR(){
+  AR = DR & 0X00FF;
+  //TODO: MOVR2, next week after RAM discussion
+}
+void MOVI(){
+   I=(DR&0x00FF);
+   D=(DR>>9);
+   D=(D&0x0003);
+   //D 0,1,2
+   GEN[D]=I;
+}
+void LODR(){
+  AR = DR & 0X00FF;
+  //TODO: LODR2, next week after RAM discussion
+}
+void GOTO(){
+  I=(DR&0x00FF);
+  PC=I;
+}
+void JMPZ(){
+    if(ZF==1){
+      I = DR & 0x00FF;
+      PC = I;
+    }
+  }
+void JMPNZ(){
+    if(ZF==0){
+      I = DR & 0x00FF;
+      PC = I;
+    }
+  }
+void NOP(){}
+void AND(){
+   D=(DR>>9);
+   D=(D&0x003);  //0,1,2
+   RL = (DR>>2); 
+   RL = RL&0003; //0,1,2
+   RR = DR;
+   RR= RR&0003; //0,1,2
+
+   GEN[D] = GEN[RL] & GEN[RR];
+   if(GEN[D]==0)
+      ZF=1; 
+   else
+      ZF=0;
+}
+void OR(){
+   D=(DR>>9);
+   D=(D&0x003);  //0,1,2
+   RL = (DR>>2); 
+   RL = RL&0003; //0,1,2
+   RR = DR;
+   RR= RR&0003; //0,1,2
+
+   GEN[D] = GEN[RL] | GEN[RR];
+   if(GEN[D]==0)
+      ZF=1; 
+   else
+      ZF=0;
+}
+void XOR(){
   
-      void LODR(){}
-      
-      void GOTO()
-      {
-        I = (DR&0x00FF);
-        PC=I;
-      }
-      
-      void JMPZ(){}
-      void JMPNZ(){}
-      void NOP(){}
-      void AND(){}
-      void OR(){}
-      void XOR(){}
-      
-      void ADD()
-      {
-        D = (DR>>9);
-        D = (D&0x0003);
-        RL =RL&0x0003;
-        RR=DR;
-        RR = RR&0x0003;
+   D=(DR>>9);
+   D=(D&0x003);  //0,1,2
+   RL = (DR>>2); 
+   RL = RL&0003; //0,1,2
+   RR = DR;
+   RR= RR&0003; //0,1,2
 
-      GEN[D]=GEN[RL]+GEN[RR];
-          if (GEN[D]==0)
-             ZF=1;
-          else
-             ZF=0;
-          }
-          
-     void SUB()
-     {
-      D = (DR>>9);
-      D = (D&0x0003);
-      RL =DR>>2;
-      RL=RL&0x0003;
-      RR=DR;
-      RR = RR&0x0003;
+   GEN[D] = GEN[RL] ^ GEN[RR];
+   if(GEN[D]==0)
+      ZF=1; 
+   else
+      ZF=0;  
+  
+}
+void ADD(){
+   D=(DR>>9);
+   D=(D&0x003);;  //0,1,2
+   RL = (DR>>2); 
+   RL = RL&0003; //0,1,2
+   RR = DR;
+   RR= RR&0003; //0,1,2
 
-      GEN[D]=GEN[RL]-GEN[RR];
-        if(GEN[D]==0)
-          ZF=1;  
-        else 
-          ZF=0;
-     }
+   GEN[D]=GEN[RL]+GEN[RR];
+   if(GEN[D]==0)
+      ZF=1; 
+   else
+      ZF=0;  
+}
+
+void SUB(){
+   D=(DR>>9);
+   D=(D&0x003);  //0,1,2
+   RL = (DR>>2); 
+   RL = RL&0003; //0,1,2
+   RR = DR;
+   RR= RR&0003; //0,1,2
+
+   GEN[D]=GEN[RL]-GEN[RR];
+   if(GEN[D]==0)
+      ZF=1; 
+   else
+      ZF=0;
+}
 
 
-
-void loop() 
-{
-  // put your main code here, to run repeatedly:
-  PC=0;
-  fetch1();
-  fetch2();
-  fetch3();
-//assume IR has the opcode
-    if (IR=1)
-      MOVR;
-    if (IR=2)
-      MOVI;
-    if (IR=3)
-      LODR;
-    if (IR=6)
-      GOTO;
-    if (IR=7)
-      JMPZ;
-    if (IR=8)
-      JMPNZ;
-    if (IR=0)
+void loop() {
+  // put your main code here, to run repeatedly:   
+   fetch1();
+   fetch2();
+   fetch3();
+   // assume IR has the opcode
+   if(IR==1)
+      MOVR();
+   if(IR==2)
+      MOVI();
+   if(IR==3)
+      LODR();
+   if(IR==6)
+      GOTO();
+   if(IR==7)
+      JMPZ();
+   if(IR==8)
+      JMPNZ();
+   if(IR==0)
       NOP();
-    if (IR=9)
+   if(IR==9)
       AND();
-    if (IR=10)
+   if(IR==10)
       OR();
-    if (IR=11)
+   if(IR==11)
       XOR();
-    if (IR=12)
+   if(IR==12)
       ADD();
-    if (IR=13)
+   if(IR==13)
       SUB();
 
-    delay (1000);
+ delay(1000);
 
+   
 }
